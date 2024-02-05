6650e463b443da6160e10d956676fc1bc06e56ad
Swann Peri--Lunal <swann.peri-lunal@epitech.eu>
2024-02-05 14:52:30 +0100

    Add Doxygen documentation

---
+++ generated_doc/html/dir_005c7b640101bdb33bd444b533a60fc0.js
@@ -0,0 +1,4 @@
+var dir_005c7b640101bdb33bd444b533a60fc0 =
+[
+    [ "my.h", "my_8h_source.html", null ]
+];
---
+++ generated_doc/html/files_dup.js
@@ -0,0 +1,4 @@
+var files_dup =
+[
+    [ "Documentation", "dir_f9dfd35b172a0e2036ace76497fb239b.html", "dir_f9dfd35b172a0e2036ace76497fb239b" ]
+];
